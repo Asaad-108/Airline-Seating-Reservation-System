af105604e54473ecd260538e033143f4d657ac11
Asaad-108 <asadasghar836@gmail.com>
2024-03-07 16:22:09 +0500

    added files

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# Airline-Seating-Reservation-System
+# Airline Seating Reservation System
+
 This C++ program simulates an airline seating reservation system where users can view the seating plan, reserve seats, and exit the program.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Usage](#usage)
+- [Installation](#installation)
+- [Dependencies](#dependencies)
+- [Contributing](#contributing)
+- [License](#license)
+- [Author](#author)
+
+## Introduction
+
+This project aims to create a simple yet effective seating reservation system for an airline. The program allows users to interactively view the seating arrangement, reserve seats according to their preferences, and gracefully exit the system when done.
+
+## Features
+
+- View seating plan categorized into first class, business class, and economy class.
+- Reserve seats by specifying the desired row and column.
+- Check seat availability and receive prompts for invalid seat selections.
+- Simple user interface with intuitive options for ease of use.
+
+## Usage
+
+1. **Compilation**:
+   - Ensure you have a C++ compiler installed on your system.
+   - Compile the `main.cpp` file using your preferred compiler.
+
+2. **Execution**:
+   - Run the compiled executable.
+   - Follow the on-screen prompts to interact with the program.
+
+3. **Seat Reservation**:
+   - Users can choose to view the seating plan, reserve seats, or exit the program.
+   - Seats are categorized into first class, business class, and economy class.
+
+## Installation
+
+To run the program, follow these steps:
+
+1. Clone the repository:
+
+
+2. Compile the `main.cpp` file using your preferred C++ compiler.
+
+3. Run the compiled executable.
+
+## Dependencies
+
+- This program depends on a `seating_plan.txt` file containing the initial seating arrangement.
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please fork the repository and submit a pull request with your changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Author
+
+- [Muhammad Asaad](https://github.com/Asaad-108?tab=repositories)
+- [asadasghar836@gmail.com]
---
+++ main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+const int rows = 13;
+const int cols = 6;
+const int frist_class = 2;
+const int business_class = 7;
+const int economy_class = 13;
+using namespace std;
+void read(ifstream &fin, char **array)
+{
+    char temp;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            fin >> temp;
+            array[i][j] = temp;
+        }
+    }
+}
+void display(char **array)
+{
+
+    cout << "\tA\tB\tC\tD\tE\tF" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "Rows " << i + 1 << "\t";
+        for (int j = 0; j < cols; j++)
+        {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+void delete_2D(char **array, int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] array[i];
+    }
+    delete[] array;
+}
+void assign_seat(char **array, int i, int j)
+{
+    array[i][j] = 'X';
+}
+int column_number(char c)
+{
+    int n = 0;
+    if (c == 'A')
+    {
+        n = 1;
+        return n;
+    }
+    else if (c == 'B')
+    {
+        n = 2;
+        return n;
+    }
+    else if (c == 'C')
+    {
+        n = 3;
+        return n;
+    }
+    else if (c == 'D')
+    {
+        n = 4;
+        return n;
+    }
+    else if (c == 'E')
+    {
+        n = 5;
+        return n;
+    }
+    else if (c == 'F')
+    {
+        n = 6;
+        return n;
+    }
+}
+int main()
+{
+    ifstream fin;
+    fin.open("seating_plan.txt");
+    char **array = NULL;
+    array = new char *[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        array[i] = new char[cols];
+    }
+    read(fin, array);
+    char choice;
+    int csss = 0;
+    int num = 0;
+
+    do
+    {
+        cout << "press 1: to see the seating plan" << endl;
+        cout << "press 2: to reserved a seat you want" << endl;
+        cout << "press 3: to exit the program" << endl;
+        cin >> num;
+        if (num == 1)
+        {
+            cout << " * means the seat is aviable and X means the seat is already reserved  " << endl;
+            display(array);
+        }
+        else if (num == 2)
+        {
+            cout << "press 1: for Frist class (rows 1-2)" << endl;
+            cout << "press 2: for Business class (rows 3-7)" << endl;
+            cout << "press 3: for Economy class (rows 8-13)" << endl;
+            cin >> csss;
+
+            int r = 0;
+            int c = 0;
+            char a;
+            cout << "enter the seat number you want " << endl;
+            cout << "enter the row number" << endl;
+            cin >> r;
+            cout << "enter the column " << endl;
+            cin >> a;
+            c = column_number(a);
+            if (csss == 1 && r <= 0 || csss == 1 && r > 2 || csss == 2 && r <= 2 && csss == 2 && r > 7 || csss == 3 && r <= 7 || csss == 3 && r > 13)
+            {
+                cout << "invalid seat selection :" << endl;
+                break;
+            }
+
+            r = r - 1;
+            c = c - 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (array[r][c] == '*')
+                    {
+                        assign_seat(array, r, c);
+                        cout << "your seat is reserved:" << endl;
+                        break;
+                    }
+                    else if (array[r][c] == 'X')
+                    {
+                        cout << "seat is already reserved tryanother seat:" << endl;
+                        break;
+                    }
+                    else
+                    {
+                        cout << "invalid option" << endl;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        else if (num == 3)
+        {
+            break;
+        }
+        else
+        {
+            cout << "invalid choice" << endl;
+        }
+        cout << "do you want to continue ? if yes press y / if no press n:" << endl;
+        cin >> choice;
+    } while (choice == 'Y' || choice == 'y');
+    cout << "the program is ended" << endl;
+
+    delete_2D(array, rows);
+    return 0;
+}
